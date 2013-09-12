1e30eeebe03abdcac04493dbda974816e34d0385
NuorAin <nuorain@amtis.com.my>
2013-09-12 12:11:34 +0800

    Interface

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,19 +2,19 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Leave Application System</title>
 
 	<style type="text/css">
 
-	::selection{ background-color: #E13300; color: white; }
-	::moz-selection{ background-color: #E13300; color: white; }
-	::webkit-selection{ background-color: #E13300; color: white; }
+	::selection{ background-color: #CCEEFF; color: white; }
+	::moz-selection{ background-color: #CCEEFF; color: white; }
+	::webkit-selection{ background-color: #CCEEFF; color: white; }
 
 	body {
-		background-color: #fff;
+		background-color: #CCEEFF;
 		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
+		font: 18px/20px normal Helvetica, Arial, sans-serif;
+		color: #01030B;
 	}
 
 	a {
@@ -24,25 +24,15 @@
 	}
 
 	h1 {
-		color: #444;
+		color: #01030B;
 		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
+		border-bottom: 2px solid #D0D0D0;
 		font-size: 19px;
 		font-weight: normal;
 		margin: 0 0 14px 0;
 		padding: 14px 15px 10px 15px;
 	}
 
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
 
 	#body{
 		margin: 0 15px 0 15px;
@@ -50,8 +40,8 @@
 	
 	p.footer{
 		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
+		font-size: 14px;
+		border-top: 2px solid #D0D0D0;
 		line-height: 32px;
 		padding: 0 10px 0 10px;
 		margin: 20px 0 0 0;
@@ -59,7 +49,7 @@
 	
 	#container{
 		margin: 10px;
-		border: 1px solid #D0D0D0;
+		border: 2px solid #1083C5;
 		-webkit-box-shadow: 0 0 8px #D0D0D0;
 	}
 	</style>
@@ -67,18 +57,17 @@
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to Leave Application System!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>Regulation Of Leave Application</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>
+			<ol>Emergency leave can't exceed 2 days.</ol>
+			<ol>Maternity leave can't exceed more than 2 months.</ol>
+			<ol>Annual Leave can't exceed as stated in the company regulation.</ol>
+			<ol>User cannot apply leave on the date before.</ol>
+		</p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
