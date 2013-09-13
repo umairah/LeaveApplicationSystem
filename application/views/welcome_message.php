63230335a1e9ca4f6bbb2550a48325a17d87d207
NuorAin <nuorain@amtis.com.my>
2013-09-13 09:08:05 +0800

    welcome

---
+++ application/views/welcome_message.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Leave Application System</title>
+
+	<style type="text/css">
+
+	::selection{ background-color: #CCEEFF; color: white; }
+	::moz-selection{ background-color: #CCEEFF; color: white; }
+	::webkit-selection{ background-color: #CCEEFF; color: white; }
+
+	body {
+		background-color: #CCEEFF;
+		margin: 40px;
+		font: 18px/20px normal Helvetica, Arial, sans-serif;
+		color: #01030B;
+	}
+
+	h1 {
+		color: #01030B;
+		background-color: transparent;
+		border-bottom: 2px solid #D0D0D0;
+		font-size: 19px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
+
+
+	#body{
+		margin: 0 15px 0 15px;
+	}
+	
+	p.footer{
+		text-align: center;
+		font-size: 12px;
+		border-top: 2px solid #D0D0D0;
+		line-height: 32px;
+		padding: 0 10px 0 10px;
+		margin: 20px 0 0 0;
+	}
+	
+	#container{
+		margin: 10px;
+		border: 2px solid #1083C5;
+		-webkit-box-shadow: 0 0 8px #D0D0D0;
+		align: center;
+	}
+	</style>
+</head>
+<body>
+<marquee>Welcome to Leave Application System!</marquee>
+<br><br>
+<a href = "category.php"><img alt="profile" src="login.jpg" width="100" length="100"></a>
+<center><img alt="company" src="comp.JPG"></center>
+
+<div id="container">
+	<h1>Regulation Of Leave Application</h1>
+
+	<div id="body">
+
+		<p>
+			<ol>
+			<li>Emergency leave can't exceed 2 days.</li>
+			<li>Maternity leave can't exceed more than 2 months.</li>
+			<li>Annual Leave can't exceed as stated in the company regulation.</li>
+			<li>User cannot apply leave on the date before.</li>
+			</ol>
+		</p>
+	</div>
+
+	<p class="footer">Copyright 2013 <b> Junior Programmer</b></p>
+</div>
+
+</body>
+</html>
